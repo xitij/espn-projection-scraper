310535798a4d0b22334c421031fb12c779175a77
Tarik <tarik@gameontechnology.com>
2020-08-31 10:01:31 -0700

    Added mappings file which map positions, teams, and stats to ESPN ids.

---
+++ src/mappings/teams.js
@@ -0,0 +1,124 @@
+/**
+ * Maps position enum to readable player position
+ */
+const positionMapping = {
+  1: 'QB',
+  2: 'RB',
+  3: 'WR',
+  4: 'TE',
+  5: 'K',
+  16: 'D/ST',
+};
+
+/**
+ * Maps team numerical enum to readable team names.
+ */
+const teamNameMapping = {
+  '-1': 'Bye',
+  1: 'Atlanta Falcons',
+  2: 'Buffalo Bills',
+  3: 'Chicago Bears',
+  4: 'Cincinnati Bengals',
+  5: 'Cleveland Browns',
+  6: 'Dallas Cowboys',
+  7: 'Denver Broncos',
+  8: 'Detroit Lions',
+  9: 'Green Bay Packers',
+  10: 'Tennessee Titans',
+  11: 'Indianapolis Colts',
+  12: 'Kansas City Chiefs',
+  13: 'Oakland Raiders',
+  14: 'Los Angeles Rams',
+  15: 'Miami Dolphins',
+  16: 'Minnesota Vikings',
+  17: 'New England Patriots',
+  18: 'New Orleans Saints',
+  19: 'New York Giants',
+  20: 'New York Jets',
+  21: 'Philadelphia Eagles',
+  22: 'Arizona Cardinals',
+  23: 'Pittsburgh Steelers',
+  24: 'Los Angeles Chargers',
+  25: 'San Francisco 49ers',
+  26: 'Seattle Seahawks',
+  27: 'Tampa Bay Buccaneers',
+  28: 'Washington Redskins',
+  29: 'Carolina Panthers',
+  30: 'Jacksonville Jaguars',
+  33: 'Baltimore Ravens',
+  34: 'Houston Texans',
+};
+
+/**
+ * Maps team numerical enum to readable team name abbreviations.
+ */
+const teamAbbrevMapping = {
+  '-1': 'Bye',
+  1: 'ATL',
+  2: 'BUF',
+  3: 'CHI',
+  4: 'CIN',
+  5: 'CLE',
+  6: 'DAL',
+  7: 'DEN',
+  8: 'DET',
+  9: 'GB',
+  10: 'TEN',
+  11: 'IND',
+  12: 'KC',
+  13: 'OAK',
+  14: 'LAR',
+  15: 'MIA',
+  16: 'MIN',
+  17: 'NE',
+  18: 'NO',
+  19: 'NYG',
+  20: 'NYJ',
+  21: 'PHI',
+  22: 'ARI',
+  23: 'PIT',
+  24: 'LAC',
+  25: 'SF',
+  26: 'SEA',
+  27: 'TB',
+  28: 'WSH',
+  29: 'CAR',
+  30: 'JAX',
+  33: 'BAL',
+  34: 'HOU',
+};
+
+/**
+ * All possible injury statuses for a Player returned by the API
+ * @typedef {
+ *   'ACTIVE' |
+ *   'BEREAVEMENT' |
+ *   'DAY_TO_DAY' |
+ *   'DOUBTFUL' |
+ *   'FIFTEEN_DAY_DL' |
+ *   'INJURY_RESERVE' |
+ *   'OUT' |
+ *   'PATERNITY' |
+ *   'PROBABLE' |
+ *   'QUESTIONABLE' |
+ *   'SEVEN_DAY_DL' |
+ *   'SIXTY_DAY_DL' |
+ *   'SUSPENSION' |
+ *   'TEN_DAY_DL'
+ * } INJURY_STATUSES
+ */
+
+/**
+ * The status of a player for fantasy rostering purposes.
+ * @typedef {
+ * 'FREEAGENT' |
+ * 'ONTEAM' |
+ * 'WAIVERS'
+ * } PLAYER_AVAILABILITY_STATUSES
+ */
+
+module.exports = {
+  positionMapping,
+  teamAbbrevMapping,
+  teamNameMapping,
+};
