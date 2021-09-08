b595d8a4373d73267555311b9fe483fb38863f5e
Tarik <tlourchane@gmail.com>
2021-09-08 00:00:56 -0700

    Added mapping for SCTID teamId to Manager names.

---
+++ src/mappings/sctid-managers.js
@@ -0,0 +1,18 @@
+const managers = {
+  1: 'Toe',
+  2: 'Hoot',
+  3: 'Kevin',
+  4: 'Me',
+  6: 'Mel',
+  8: 'Lou',
+  9: 'Chapman',
+  10: 'Elway',
+  11: 'King',
+  12: 'Gak',
+  13: 'Aco',
+  14: 'Borg',
+};
+
+module.exports = {
+  managers,
+};
